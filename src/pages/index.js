ba7f1ac0804cd04e25e5c9b26701f0b4d66699e5
Eric Buchholz <eric.buchholz@deutschebahn.com>
2021-05-19 16:03:38 +0200

    Update page data

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/README">
-            Docusaurus Tutorial - 5min ⏱️
+            Documentation 📚️
           </Link>
         </div>
       </div>
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description="Documentation to integrate DB Consent Layer on your website">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
